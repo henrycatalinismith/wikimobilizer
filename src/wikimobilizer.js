e8ad75c6b00e87e3ff49c232388094a556cdc816
Henry Catalini Smith <henry@catalinismith.se>
2015-02-22 11:44:36 +0000

    At long last get the damn listener in place correctly

--- src/wikimobilizer.js
+++ src/wikimobilizer.js
@@ -1,3 +1,10 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
+var listener = function(details) {
   console.log(details.url);
-});
+  return {
+    redirectUrl: 'http://poop.bike'
+  };
+};
+
+var filter = { urls : ["<all_urls>"] };
+
+chrome.webRequest.onBeforeRequest.addListener(listener, filter, ["blocking"]);

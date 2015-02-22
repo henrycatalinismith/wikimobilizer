a183723fcd50649b96d8ac54fcf1f0dcaf7c97df
Henry Catalini Smith <henry@catalinismith.se>
2015-02-22 12:02:40 +0000

    Tidy up the API and namespace it

--- src/wikimobilizer.js
+++ src/wikimobilizer.js
@@ -1,23 +1,29 @@
-var listener = function(details) {
-  return {
-    redirectUrl: redirect(details.url)
-  };
+var Wikimobilizer = {
+
+  listen: function(event) {
+    var filter = { urls : ["<all_urls>"] };
+    event.addListener(this.redirect.bind(this), filter, ["blocking"]);
+  },
+
+  redirect: function(details) {
+    return {
+      redirectUrl: this.transformUrl(details.url)
+    };
+  },
+
+  transformUrl: function(url) {
+    var regex = /http:\/\/en.wikipedia.org\/wiki\/([^\/]+)/;
+    var match = url.match(regex);
+    if (match) {
+      url = 'http://en.m.wikipedia.org/wiki/' + match[1];
+    }
+    return url;
+  }
+
 };
 
-var regex = /http:\/\/en.wikipedia.org\/wiki\/([^\/]+)/;
-
-var redirect = function(url) {
-  var match = url.match(regex);
-  if (match) {
-    url = 'http://en.m.wikipedia.org/wiki/' + match[1];
-  }
-  return url;
-}
-
-var filter = { urls : ["<all_urls>"] };
-
 if (typeof chrome !== 'undefined' && typeof chrome.webRequest !== 'undefined') {
-  chrome.webRequest.onBeforeRequest.addListener(listener, filter, ["blocking"]);
+  Wikimobilizer.listen(chrome.webRequest.onBeforeRequest);
 } else if (typeof module !== 'undefined') {
-  module.exports = redirect;
+  module.exports = Wikimobilizer;;
 }
